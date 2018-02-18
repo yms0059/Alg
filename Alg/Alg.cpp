40430d7338b22fcef27ed8b2ae2919c570feaad8
윤명식 <yms0059@naver.com>
2018-02-18 16:43:36 +0900

    팩토리얼

--- Alg/Alg.cpp
+++ Alg/Alg.cpp
@@ -1,21 +1,23 @@
 #include<stdio.h>
 
-int sum(int n);
+int factorial(int n);
 
 int main()
 {
 	int number;
 	printf("숫자를 입력하세요 : ");
 	scanf("%d", &number);
-	printf("1부터 %d까지의 합 : %d\n", number, sum(number));
-
-	return 0;
+	printf("%d의 팩토리얼 : %d\n", number, factorial(number));
 }
 
-// 합을 구하는 재귀함수
-int sum(int n)
+// factorial
+int factorial(int n)
 {
-	if (n <= 0)
-		return 0;
-	return n + sum(n - 1);
+	if (n <= 0)return 0;
+	else if (n==1){
+		return 1;
+	}
+	else {
+		return n*factorial(n - 1);
+	}
 }
