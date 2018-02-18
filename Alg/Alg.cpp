f7b880a43b4a95336ae5c8bd5f75b4cca6bdc063
윤명식 <yms0059@naver.com>
2018-02-18 17:07:20 +0900

    바이너리서치

--- Alg/Alg.cpp
+++ Alg/Alg.cpp
@@ -1,42 +1,43 @@
 #include<stdio.h>
 
-int gcd(int m,int n);
+int BinarySearch(int nArray[], int begin, int end, int target);
 
 int main()
 {
-	int m, n;//두수 입력
-	int i;
-	printf("숫자를 입력하세요 : ");
-	scanf("%d", &m);
-	scanf("%d", &n);
-	
+	int nArray[] = { 1,3,5,7,9,11,13 };
+	int result;
 
+	result = BinarySearch(nArray, 0, sizeof(nArray) / sizeof(int) - 1, 7);
+
+	if (result==-1) {
+		printf("찾는 값이 없습니다");
+	}
+	else {
+		for (int i = 0; i < sizeof(nArray) / sizeof(int); i++) {
+			printf("%d", nArray[i]);
+		}
+		printf("\n%d 번쨰 배열 요소에 있습니다.", result);
+	}
 	return 0;
-
 }
 
-// 최대 공약수
-int gcd(int m,int n)
+// 바이너리서치
+int BinarySearch(int nArray[],int begin,int end,int target)
 {
-	if (m < n) {
-		int tmp = m;
-		m = n;
-		n = tmp;
+	int mid=0;
+	if (begin > end) {
+		return -1;
 	}
-	if (m%n == 0) {
-		return n;
+	mid = (begin + end) / 2;
+
+	if (nArray[mid] == target) {
+		return mid;
+	}
+	else if (target < nArray[mid]) {
+		return BinarySearch(nArray, begin, mid - 1, target);
 	}
 	else {
-		return gcd(n, m%n);
+		return BinarySearch(nArray, mid+1, end, target);
 	}
 }
 
-int gcds(int m, int n)
-{
-	if (m == 0) {
-		return n;
-	}
-	else {
-		return gcd(n, m%n);
-	}
-}
