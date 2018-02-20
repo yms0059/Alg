e5b1db52a4ab11407432a1e418f3d1e153fc4569
윤명식 <yms0059@naver.com>
2018-02-20 17:03:33 +0900

    힙정렬-상향식힙생성

--- Alg/Alg.cpp
+++ Alg/Alg.cpp
@@ -1,6 +1,7 @@
 #include <stdio.h>
 
-/*
+//상향식 힙생성
+/* 
    힙을 배열로
    가장큰 key값을 root로 한다 자식은 부모원소보다 작은 값이 들어가며 
    힙은 이진트리를 가져야한다
@@ -19,12 +20,6 @@ int n = 0;  //힙에 key값이 들어가면 증가
 int i, key;
 char  choice;
 
-void insertItem(int key);
-int removeMax();
-void upHeap(int i);
-void downHeap(int i);
-void printHeap();
-
 
 int main()
 {
@@ -32,64 +27,31 @@ int main()
 
 	while (1)
 	{
-		scanf("%c", &choice);
-		switch (choice)
-		{
-		case  'i':
-			scanf("%d", &key);
-			insertItem(key);
-			printf("0\n");
-			break;
-		case 'd':
-			key = removeMax();
-			printf("%d\n", key);
-			break;
-		case 'p':
-			printHeap();
-			printf("\n");
-			break;
-		case 'q':
-			//exit(1);
-			break;
+		scanf("%d", &n);
 
+		for (i = 1; i <= n; i++)
+		{
+			scanf("%d", &H[i]);
 		}
+		buildHeap();
+		printHeap();
+		printf("\n");
+		break;
 	}
 	return 0;
 }
 
-void insertItem(int key)
+void buildHeap()
 {
-	(n)++;
-	H[n] = key;
-	upHeap(n);
-
+	int i;
+	for (i = n / 2; i >= 1; i--)
+		downHeap(i);
 }
 
-void upHeap(int i)
-{
-	int k = H[i];
-	int par = i / 2; //나누기 2인이유 부모를 비교하기위해
-	while (H[par] < k)
-	{
-		H[i] = H[par];
-		i = par;
-		par = i / 2;
-	}
-	H[i] = k;
-}
-
-int removeMax()
-{
-	key = H[1];//인덱스1번의 값이 가장큰값이기 때문에
-	H[1] = H[n];//마지막인덱스의 key값을 root에 가져다놓고
-	(n)--;     //마지막 key값이 저장된 위치를 삭제
-	downHeap(1);//1번 인덱스부터 재배열 하기위해서 가즈아
-	return key; //삭제된 key값을 리턴(가장큰 값을 리턴 즉 root값 리턴)
-}
 
 void downHeap(int i)
 {
-	int lchild = 2 * i, rchild = lchild + 1; 
+	int lchild = 2 * i, rchild = lchild + 1; //
 	int tmp = H[i];
 	
 	while (rchild <= n)
